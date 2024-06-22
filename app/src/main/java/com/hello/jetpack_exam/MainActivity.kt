498616c486d2ed0f564e3b7218d8324e9abf5b47
foxworld <foxworld@gmail.com>
2024-06-22 10:08:49 +0900

    card image 구현

--- app/src/main/java/com/hello/jetpack_exam/MainActivity.kt
+++ app/src/main/java/com/hello/jetpack_exam/MainActivity.kt
@@ -1,47 +1,104 @@
+/*
+ * Copyright© 2024. KSNET INC, All rights reserved.
+ * @Company : KSNET INC.
+ * @Name : PETER HOON LEE
+ * @TelNo : 02-3420-5844
+ * @Email : foxworld@ksnet.co.kr
+ * @Created Date : 2024-6-22
+ */
+
 package com.hello.jetpack_exam
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.hello.jetpack_exam.ui.theme.Jetpack_examTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Jetpack_examTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            var isFavorite by rememberSaveable {
+                mutableStateOf(false)
+            }
+
+            ImageCard(
+                modifier = Modifier
+                    .fillMaxWidth(1.0f)
+                    .padding(20.dp),
+                isFavorite = isFavorite,
+            ) {
+                favorite ->
+                isFavorite = !isFavorite
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun ImageCard(
+    modifier: Modifier = Modifier,
+    isFavorite: Boolean,
+    onTabFavorite: (Boolean) -> Unit
+) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Jetpack_examTheme {
-        Greeting("Android")
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 6.dp)
+    ) {
+        Box(
+            modifier = Modifier.height(200.dp),
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.flower),
+                contentDescription = "flower",
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.TopEnd,
+            ) {
+                IconButton(onClick = {
+                    onTabFavorite.invoke(isFavorite)
+                }) {
+                    Icon(
+                        imageVector = if(isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                        contentDescription = "favorite",
+                        tint = Color.White
+                    )
+                }
+
+
+            }
+        }
     }
 }
+
