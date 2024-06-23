ef4b46abcca07d68b07cb803f426b0ee23edc730
foxworld <foxworld@gmail.com>
2024-06-23 15:15:22 +0900

    Scaffold 구현중

--- app/src/main/java/com/hello/jetpack_exam/MainActivity.kt
+++ app/src/main/java/com/hello/jetpack_exam/MainActivity.kt
@@ -14,7 +14,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -23,14 +25,19 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -39,23 +46,55 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            //val textValue = remember {
+            val (text, setValue) = remember {
+                mutableStateOf("")
+            }
             var isFavorite by rememberSaveable {
                 mutableStateOf(false)
             }
 
-            ImageCard(
-                modifier = Modifier
-                    .fillMaxWidth(1.0f)
-                    .padding(20.dp),
-                isFavorite = isFavorite,
-            ) {
-                favorite ->  isFavorite = !isFavorite
+            val scaffoldState = rememberScaffoldState()
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+
+            ) { innerPadding ->
+                ImageCard(
+                    modifier = Modifier
+                        .fillMaxWidth(1.0f)
+                        .padding(8.dp)
+                        .padding(innerPadding),
+                    isFavorite = isFavorite,
+
+                ) { favorite ->
+                    isFavorite = !isFavorite
+                }
+
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        TextField(
+                            //value = textValue.value,
+                            //onValueChange = { textValue.value = it }
+                            value = text,
+                            onValueChange = setValue
+                        )
+                        Button(onClick = { }) {
+                            Text("클릭")
+                        }
+                    }
+                }
             }
         }
     }
